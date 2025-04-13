c5e7eeadfeb8132b684f80cc87f4b9589e1d936f
aliceof <shuixian789@qq.com>
2025-04-13 23:02:28 +0800

    4-13

--- transformer/src/main/java/com/alice/test1/attention/MatrixUtils.java
+++ transformer/src/main/java/com/alice/test1/attention/MatrixUtils.java
@@ -1,5 +1,7 @@
 package com.alice.test1.attention;
 
+import java.util.Arrays;
+
 public class MatrixUtils {
     public static double[][] dot(double[][] a,double[][]b){//a是query矩阵，b是转置矩阵
         int m=a.length,n=b[0].length,k=a[0].length;
@@ -24,6 +26,30 @@ public class MatrixUtils {
         return transposed;
     }
     public static double[][] softmax(double[][] input){
+        double[][] result=new double[input.length][input[0].length];
+        for (int i=0;i<input.length;i++){
+            double max= Arrays.stream(input[i]).max().orElse(0.0);
+            double sum=0;
+            for (int j=0;j<input[i].length;j++){
+                result[i][j]=Math.exp(input[i][j]-max);
+                sum+=result[i][j];
+            }
+            for (int j=0;j<input[i].length;j++){
+                result[i][j]/=sum;
+            }
+        }
+        return result;
+    }
+    public static double[][] scale(double[][] matrix,double factor){
+        double[][] result=new double[matrix.length][matrix[0].length];
+        for (int i=0;i<matrix.length;i++){
+            for (int j=0;j<matrix[i].length;j++){
+                result[i][j]=matrix[i][j]*factor;
+            }
+        }
+        return result;
+    }
+    public static double[][] spilt(double[][] matrix,int start,int end){
 
     }
-}
+ }
